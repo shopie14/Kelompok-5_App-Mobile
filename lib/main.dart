0e7263a0cfd11116a84fcfc3a26432a1933ad1e5
shopie14 <sh.pinh14@gmail.com>
2023-04-15 04:55:12 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:pertemuan_v/config/app_routes.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.white,
-      ),
-      title: "Apps Pertemuan 5",
-      routeInformationProvider: AppRoutes.goRouter.routeInformationProvider,
-      routerDelegate: AppRoutes.goRouter.routerDelegate,
-      routeInformationParser: AppRoutes.goRouter.routeInformationParser,
-    );
-  }
+import 'package:flutter/material.dart'; 
+import 'package:flutter/src/widgets/framework.dart'; 
+import 'package:flutter/src/widgets/placeholder.dart'; 
+import 'package:pertemuan_v/data/animes_data.dart'; 
+import 'package:pertemuan_v/models/anime.dart'; 
+import 'package:pertemuan_v/modul/home/header_widget.dart'; 
+import 'package:pertemuan_v/widget/news_item_widget.dart'; 
+ 
+class NewsScreen extends StatelessWidget { 
+  const NewsScreen({super.key}); 
+ 
+  @override 
+  Widget build(BuildContext context) { 
+    return ListView( 
+      children: [ 
+        const ListTile(title: Text("Anime List")), 
+        Column( 
+          children: 
+              newsData.map((News news) => NewsItemWidget(news: news)).toList(), 
+        ), 
+      ], 
+    ); 
+  } 
 }
