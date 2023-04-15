4457fded6b71ea45a7e1474c735a30e2988e920f
Fadiasitinursifa <nursifafadia2>
2023-04-15 12:02:27 +0700

    update ulang

--- lib/data/animes_data.dart
+++ lib/data/animes_data.dart
@@ -45,5 +45,18 @@ List<News> hotNews = [
         "Mahiru Shiina is worthy of her nickname Angel: she is a divine beauty loved by all, and she excels in both academics and athletics. Shiina lives in an entirely different world from Amane Fujimiya, her next-door neighbor. Despite living so close together, they have never spoken once. But their silence is broken when Fujimiya spots Shiina gloomily sitting on a swing amidst a heavy rainstorm and lends her his umbrella. When Fujimiya catches a cold the next day, Shiina wishes to return the favor for the umbrella by nursing him back to health. Believing that this would be their first and last interaction, he silently appreciates her kindness. However, Shiina—who can not help but worry about Fujimiya's lack of tidiness and proper nutrition—begins to cook and clean for him. As the unlikely pair spend time together in Fujimiya's apartment, they explore the true nature of their relationship and the gentle emotions that emerge from it",
     image: "https://cdn.myanimelist.net/images/anime/1847/134795.jpg",
   ),
+  News(
+    id: 3,
+    title: "Love Live! School Idol Project 2nd Season",
+    description:
+        "Otonokizaka High School has been saved! Despite having to withdraw from the Love Live!, the efforts of μ's were able to garner enough interest in their school to prevent it from being shut down. What more, following the conclusion of the first, a second Love Live! is announced, this time on an even larger stage than before. Given a chance for redemption, the nine girls come together once more to sing their hearts out and claim victory. However, with the end of the school year approaching, the graduation of the third years draws near.\nAs they attempt to reach the top of the Love Live!, they must also consider their future and choose what path the group will take. Though the question of whether to continue without the third years or disband weighs heavily on the minds of its members, μ's must quickly come to an answer with graduation right around the corner.\nLove Live! School Idol Project 2nd Season continues the story of the girls as they laugh, cry, sing, and dance in their journey to determine the future of their group and conquer the Love Live! in their last chance to win with all nine girls together.",
+    image: "https://cdn.myanimelist.net/images/anime/10/59101.jpg",
+  ),
+  News(
+    id: 4,
+    title: "Ayakashi Triangle",
+    description:
+        "Ayakashi are strange, supernatural creatures invisible to the majority of people. Though most are harmless, some ayakashi attack humans to devour their life force. Exorcist ninjas are tasked with protecting people from these spiteful spirits. Matsuri Kazamaki is a young exorcist ninja born to a prestigious clan. His childhood friend, Suzu Kanade, is an ayakashi medium—an individual with immense vitality that attracts ayakashi. Matsuri deems all ayakashi threatening and wishes to keep Suzu as far from them as possible, but this clashes with her desire to remain close to them. One day, the pair encounters Shirogane, the King of Ayakashi, who is hellbent on consuming Suzu.\nAs she is about to be eaten, Matsuri comes to the rescue and manages to seal the ayakashi's powers—but not before Shirogane takes revenge by turning him into a girl! Until he finds a way to transform back, Matsuri must adjust to his new, peculiar lifestyle while fighting against hostile ayakashi. Moreover, Suzu is now conflicted about her feelings toward Matsuri—further plunging their relationship into a state of ambiguity.",
+    image: "https://cdn.myanimelist.net/images/anime/1078/131921.jpg",
+  ),
 ];
-
