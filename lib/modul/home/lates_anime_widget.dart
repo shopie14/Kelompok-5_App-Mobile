92a6ae6cce86675376cc7e4278f58b31268edf2c
rikalestari1411 <2006173@itg.ac.id>
2023-04-15 11:42:42 +0700

    lates_anime

--- lib/modul/home/lates_anime_widget.dart
+++ lib/modul/home/lates_anime_widget.dart
@@ -3,34 +3,63 @@ import 'package:pertemuan_v/models/anime.dart';
 
 import '../../data/animes_data.dart';
 import '../../widget/news_item_widget.dart';
+import '../anime_list/anime_screen.dart';
 
 class LatesNewsWidget extends StatelessWidget {
   const LatesNewsWidget({
     super.key,
   });
 
+  Future<List<News>> getdataNews() async {
+    List<News> newslist = [];
+    await Future.delayed(const Duration(seconds: 2), () {
+      newslist = newsData;
+    });
+    return newslist;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         ListTile(
-          title: const Text("Lates Anime",
-              style: TextStyle(fontWeight: FontWeight.bold)),
+          title: const Text("Lates News"),
           trailing: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text("News"),
+                    ),
+                    body: const NewsScreen(),
+                  );
+                }));
+              },
               icon: const Icon(
                 Icons.arrow_forward_ios,
                 size: 15,
               )),
         ),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 5),
-          child: Column(
-              children: List.generate(
-                  3,
-                  (int i) => NewsItemWidget(
-                        news: newsData[i],
-                      ))),
+        FutureBuilder(
+          future: getdataNews(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                child: Column(
+                    children: List.generate(
+                        3,
+                        (int i) => NewsItemWidget(
+                              news: newsData[i],
+                            ))),
+              );
+            }
+          },
         )
       ],
     );
